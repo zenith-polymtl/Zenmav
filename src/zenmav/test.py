5caf0bad818504c83f760d6d9a96cc18017a4aa8
Astro-Coco <colinc131@gmail.com>
2025-08-01 10:39:52 -0400

    fixed gps bug, replaced guided arm takeoff

--- src/zenmav/test.py
+++
@@ -1,7 +0,0 @@
-from zenmav.core import Zenmav
-
-nav = Zenmav()  # Example threshold of 5 meters
-nav.guided_arm_takeoff(height=20)  # Example height of 20 meters
-
-target = nav.convert_to_global(local_pos=(10, 10))  # Example local position
-nav.global_target(target.append(10))
